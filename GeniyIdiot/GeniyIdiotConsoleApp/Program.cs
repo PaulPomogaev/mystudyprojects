08f698f4bdbc14359872e605afb2f0b9342070bb
Paul Pomogaev <paulslock1@gmail.com>
2025-03-05 17:26:26 +0400

    убрал else после if в TestRepeat

--- GeniyIdiot/GeniyIdiotConsoleApp/Program.cs
+++ GeniyIdiot/GeniyIdiotConsoleApp/Program.cs
@@ -93,10 +93,7 @@ namespace GeniyIdiotConsoleApp
 				{
 					return false;
 				}
-				else
-				{
-					Console.WriteLine("Введите либо 'да' либо 'нет'!");
-				}
+				Console.WriteLine("Введите либо 'да' либо 'нет'!");
 			}
 		}
 		private static int GetUserAnswer()
