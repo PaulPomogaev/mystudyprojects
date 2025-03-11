ad3b5b9d36d921ad10c1a9d4548e7c61d27e8417
Paul Pomogaev <paulslock1@gmail.com>
2025-03-11 15:00:55 +0400

    создал классы Question, QuestionsStorage, User, UsersResultStorage
    изменил код в Main для работы с классами

--- GeniyIdiot/GeniyIdiotConsoleApp/Program.cs
+++ GeniyIdiot/GeniyIdiotConsoleApp/Program.cs
@@ -6,145 +6,188 @@ using System.Xml.Linq;
 
 namespace GeniyIdiotConsoleApp
 {
-	internal class Program
-	{
-		private const string TableRowFormat = "| {0, -30} | {1, 25} | {2, -10} |";
+    public class Question
+    {
+        public string Text { get; set; }
+        public int Answer { get; set; }
 
-		private const string TableSeparator = "---------------------------------------------------------------------------";
+        public Question(string text, int answer)
+        {
+            Text = text;
+            Answer = answer;
+        }
+    }
 
-        static void Main(string[] args)
-		{
-			bool repeat;
-			do
-			{
-				Console.WriteLine($"Здравствуйте, введите своё имя");
-				string name = Console.ReadLine();
+    public class QuestionsStorage
+    {
+        public List<Question> GetAllQuestions()
+        {
+            return new List<Question>
+            {
+            new Question("Сколько будет два плюс два умноженное на два?", 6),
+            new Question("Бревно нужно распилить на 10 частей, сколько нужно сделать распилов?", 9),
+            new Question("На двух руках 10 пальцев, сколько пальцев на 5 руках?", 25),
+            new Question("Укол делают каждые пол часа, сколько нужно минут для трёх уколов?", 60),
+            new Question("Пять свечей горело, две потухли, сколько свечей осталось?", 2)
+            };
+        }
+    }
 
-				List<string> questions = GetQuestions();
+    public class User
+    {
+        public string Name { get; set; }
 
-				List<int> answers = GetAnswers();
+        public User(string name)
+        {
+            Name = name;
+        }
+    }
 
-				if (questions.Count != answers.Count)
-				{
-					Console.WriteLine("Ошибка конфигурации теста. Количество вопросов не соотвествует количеству ответов");
-					return;
-				}
-				int questionsCount = questions.Count;
+    public class UsersResultStorage
+    {
+        string resultsPath = "results.csv";
+        public void SaveTestResult(TestResult result)
+        {
+            bool needHeader = !File.Exists(resultsPath);
 
-				int rightAnswersCount = 0;
 
-				var random = new Random();
-
-				for (int i = 0; i < questionsCount; i++)
-				{
-					Console.WriteLine($"Вопрос номер {i + 1}");
-					var randomQuestionIndex = random.Next(questions.Count);
-					Console.WriteLine(questions[randomQuestionIndex]);
-
-					int userAnswer = GetUserAnswer();
-					int rightAnswer = answers[randomQuestionIndex];
-
-					if (userAnswer == rightAnswer) 
-					{
-						rightAnswersCount++;
-					}
-					questions.RemoveAt(randomQuestionIndex);
-					answers.RemoveAt(randomQuestionIndex);
+            using (StreamWriter writer = new StreamWriter(resultsPath, true))
+            {
+                if (needHeader)
+                {
+                    writer.WriteLine("Name,CorrectAnswers,Diagnosis");
                 }
 
-				Console.WriteLine($"{name}, количество правильных ответов: {rightAnswersCount}");
+                writer.WriteLine($"{result.User.Name},{result.CorrectAnswers},{result.Diagnosis}");
 
-				var diagosisCalculator = new DiagnosesCalculation();
-				string diagnosis = diagosisCalculator.GetResults(rightAnswersCount, questionsCount);
+            }
+        }
+    }
+
+    public class TestResult
+    {
+        public User User { get; set; }
+        public int CorrectAnswers { get; set; }
+        public string Diagnosis { get; set; }
+
+        public TestResult(User name, int correctAnswers, string diagnosis)
+        {
+            User = name;
+            CorrectAnswers = correctAnswers;
+            Diagnosis = diagnosis;
+        }
+    }
+
+    internal class Program
+    {
+        private const string TableRowFormat = "| {0, -30} | {1, 25} | {2, -10} |";
+
+        private const string TableSeparator = "---------------------------------------------------------------------------";
+
+        static void Main(string[] args)
+        {
+            bool repeat;
+            var questionsStorage = new QuestionsStorage();
+            var resultsStorage = new UsersResultStorage();
+            do
+            {
+                Console.WriteLine($"Здравствуйте, введите своё имя");
+                string name = Console.ReadLine();
 
 
-				Console.WriteLine($"{name}, ваш диагноз: {diagnosis}");
+                List<Question> questions = questionsStorage.GetAllQuestions();
+                List<Question> currentTestQuestions = new List<Question>(questions);
 
-				var testResult = new TestResult
-				{
-					Name = name,
-					CorrectAnswers = rightAnswersCount,
-					Diagnosis = diagnosis
-				};
+                int rightAnswersCount = 0;
 
-				SaveTestResult(testResult);
+                var random = new Random();
 
-				repeat = TestRepeat();
-			} while (repeat);
+                for (int i = 0; i < questions.Count; i++)
+                {
+                    Console.WriteLine($"Вопрос номер {i + 1}");
+                    var randomQuestionIndex = random.Next(currentTestQuestions.Count);
+                    Question currentQuestion = currentTestQuestions[randomQuestionIndex];
+                    Console.WriteLine(currentQuestion.Text);
 
-			if (AskToShowResults())
-			{
-				ShowTestResults();
-			}
+                    int userAnswer = GetUserAnswer();
+                    int rightAnswer = currentQuestion.Answer;
 
-			Console.WriteLine("Спасибо за участие! До свидания!");
-			Console.ReadKey();
-		}
-		static bool TestRepeat()
-		{
-			while(true)
-			{
-				Console.WriteLine("Хотите пройти тест ещё раз? (да/нет)");
-				var userRespond = Console.ReadLine()?.Trim().ToLower();
-				if (userRespond == "да")
-				{
-					return true;
-				}
-				if (userRespond == "нет")
-				{
-					return false;
-				}
-				Console.WriteLine("Введите либо 'да' либо 'нет'!");
-			}
-		}
-		private static int GetUserAnswer()
-		{
-			while (true)
-			{
-				Console.WriteLine("Введите ответ в виде целого числа");
-				string userAnswer = Console.ReadLine();
+                    if (userAnswer == rightAnswer)
+                    {
+                        rightAnswersCount++;
+                    }
+                    currentTestQuestions.RemoveAt(randomQuestionIndex);
+                }
 
-				if (int.TryParse(userAnswer, out int parsedNumber))
-				{
-					return parsedNumber;
-				}
+                Console.WriteLine($"{name}, количество правильных ответов: {rightAnswersCount}");
 
-				Console.WriteLine($"Ответ не соответствует заданному диапазону от -2*10^9 до 2*10^9. Пожалуйста, повторите ввод.");
-			}
-		}
-		
-		static List<string> GetQuestions()
-		{
-			return new List<string>
-			{
-			"Сколько будет два плюс два умноженное на два?",
-			"Бревно нужно распилить на 10 частей, сколько нужно сделать распилов?",
-			"На двух руках 10 пальцев, сколько пальцев на 5 руках?",
-			"Укол делают каждые пол часа, сколько нужно минут для трёх уколов?",
-			"Пять свечей горело, две потухли, сколько свечей осталось?"
-		    };
-		}
+                var diagosisCalculator = new DiagnosesCalculation();
+                string diagnosis = diagosisCalculator.GetResults(rightAnswersCount, questions.Count);
 
-		static List<int> GetAnswers()
-		{
-			return new List<int>
-			{ 
-			   6,
-			   9,
-			   25,
-			   60,
-			   2
-		    };
-		}
 
-		public class DiagnosesCalculation
-		{
-			private List<string> diagnosis;
+                Console.WriteLine($"{name}, ваш диагноз: {diagnosis}");
 
-			public DiagnosesCalculation()
-			{
-				diagnosis = new List<string>()
-				{
+                var testResult = new TestResult
+                (
+                    new User(name),
+                    rightAnswersCount,
+                    diagnosis
+                );
+
+                resultsStorage.SaveTestResult(testResult);
+
+                repeat = TestRepeat();
+            } while (repeat);
+
+            if (AskToShowResults())
+            {
+                ShowTestResults();
+            }
+
+            Console.WriteLine("Спасибо за участие! До свидания!");
+            Console.ReadKey();
+        }
+        static bool TestRepeat()
+        {
+            while (true)
+            {
+                Console.WriteLine("Хотите пройти тест ещё раз? (да/нет)");
+                var userRespond = Console.ReadLine()?.Trim().ToLower();
+                if (userRespond == "да")
+                {
+                    return true;
+                }
+                if (userRespond == "нет")
+                {
+                    return false;
+                }
+                Console.WriteLine("Введите либо 'да' либо 'нет'!");
+            }
+        }
+        private static int GetUserAnswer()
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите ответ в виде целого числа");
+                string userAnswer = Console.ReadLine();
+
+                if (int.TryParse(userAnswer, out int parsedNumber))
+                {
+                    return parsedNumber;
+                }
+
+                Console.WriteLine($"Ответ не соответствует заданному диапазону от -2*10^9 до 2*10^9. Пожалуйста, повторите ввод.");
+            }
+        }
+
+        public class DiagnosesCalculation
+        {
+            private List<string> diagnosis;
+
+            public DiagnosesCalculation()
+            {
+                diagnosis = new List<string>()
+                {
                     "Идиот",
                     "Кретин",
                     "Дурак",
@@ -152,131 +195,104 @@ namespace GeniyIdiotConsoleApp
                     "Талант",
                     "Гений"
                 };
-				
-			}
 
-			public string GetResults(int correctAnswers, int totalQuestions)
-			{
-				double persentage = (double)correctAnswers / totalQuestions * 100;
+            }
 
-				// переменная persantage показывает процентное соотношение правильных ответов к количеству заданных вопросов, что определяет диагноз
-				// каждая цифра 16.66-33.33 и т.д., равномерно делит диапазон от 0% до 100% на 6 частей, каждая из которых соответствует диагнозу
-				if (persentage <= 16.66)
-					return diagnosis[0];
-				if (persentage <= 33.33)
-					return diagnosis[1];
-				if (persentage <= 50)
-					return diagnosis[2];
-				if (persentage <= 66.66)
-					return diagnosis[3];
-				if (persentage <= 83.33)
-					return diagnosis[4];
-				return diagnosis[5];
-			}
-		}
+            public string GetResults(int correctAnswers, int totalQuestions)
+            {
+                double persentage = (double)correctAnswers / totalQuestions * 100;
 
-		public class TestResult
-		{
-			public string Name { get; set; }
-			public int CorrectAnswers { get; set; }
-			public string Diagnosis { get; set; }
-		}
+                // переменная persantage показывает процентное соотношение правильных ответов к количеству заданных вопросов, что определяет диагноз
+                // каждая цифра 16.66-33.33 и т.д., равномерно делит диапазон от 0% до 100% на 6 частей, каждая из которых соответствует диагнозу
+                if (persentage <= 16.66)
+                    return diagnosis[0];
+                if (persentage <= 33.33)
+                    return diagnosis[1];
+                if (persentage <= 50)
+                    return diagnosis[2];
+                if (persentage <= 66.66)
+                    return diagnosis[3];
+                if (persentage <= 83.33)
+                    return diagnosis[4];
+                return diagnosis[5];
+            }
+        }
 
-		static void SaveTestResult(TestResult result)
-		{
-			string resultsPath = "results.csv";
+        static List<TestResult> ReadTestResults()
+        {
+            string resultsPath = "results.csv";
 
-			bool needHeader = !File.Exists(resultsPath);
+            List<TestResult> results = new List<TestResult>();
 
+            using (StreamReader reader = new StreamReader(resultsPath))
+            {
+                reader.ReadLine();
 
-            using (StreamWriter writer = new StreamWriter(resultsPath, true))
-			{
-				if (needHeader)
-				{
-					writer.WriteLine("Name,CorrectAnswers,Diagnosis");
-				}
+                string line;
 
-				writer.WriteLine($"{result.Name},{result.CorrectAnswers},{result.Diagnosis}");
-				
-			}
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] columns = line.Split(',');
 
-		}
+                    if (columns.Length != 3)
+                    {
+                        Console.WriteLine($"Ошибка в строке {line} недостаточное количество колонок");
+                        continue;
+                    }
 
-		static List<TestResult> ReadTestResults()
-		{
-			string resultsPath = "results.csv";
+                    if (!int.TryParse(columns[1], out int correctAnswers))
+                    {
+                        Console.WriteLine($"Некорректное число в строке {line}");
+                        continue;
+                    }
+                    results.Add(new TestResult
+                    (
+                        new User(columns[0].Trim()),
+                        correctAnswers,
+                        columns[2].Trim()
+                    ));
+                }
+            }
+            return results;
+        }
 
-			List<TestResult> results = new List<TestResult>();
+        static void ShowTestResults()
+        {
+            List<TestResult> results = ReadTestResults();
 
-			using (StreamReader reader = new StreamReader(resultsPath))
-			{
-				reader.ReadLine();
+            Console.WriteLine("\nПредыдущие результаты пользователей:");
+            Console.WriteLine(TableSeparator);
+            Console.WriteLine(TableRowFormat, "ФИО", "Кол-во правильных ответов", "Диагноз");
+            Console.WriteLine(TableSeparator);
+            foreach (var result in results)
+            {
+                Console.WriteLine(TableRowFormat,
+                    result.User.Name,
+                    result.CorrectAnswers,
+                    result.Diagnosis);
+            }
+            Console.WriteLine(TableSeparator);
+        }
 
-				string line;
+        static bool AskToShowResults()
+        {
+            while (true)
+            {
+                Console.WriteLine("Показать историю результатов? (да/нет)");
 
-				while ((line = reader.ReadLine()) != null)
-				{
-					string[] columns = line.Split(',');
+                var userRespond = Console.ReadLine().Trim().ToLower();
 
-					if (columns.Length != 3)
-					{
-						Console.WriteLine($"ОшибкаБ в строке {line} недостаточное количество колонок");
-						continue;
-					}
+                if (userRespond == "да")
+                {
+                    return true;
+                }
+                if (userRespond == "нет")
+                {
+                    return false;
+                }
 
-					if (!int.TryParse(columns[1], out int correctAnswers))
-					{
-						Console.WriteLine($"Некорректное число в строке {line}");
-						continue;
-					}
-					results.Add(new TestResult
-					{
-						Name = columns[0].Trim(),
-						CorrectAnswers = correctAnswers,
-						Diagnosis = columns[2].Trim()
-					});
-				}
-			}
-			return results;
-		}
-		 
-		static void ShowTestResults()
-		{
-			List<TestResult> results = ReadTestResults();
-
-			Console.WriteLine("\nПредыдущие результаты пользователей:");
-			Console.WriteLine(TableSeparator);
-			Console.WriteLine(TableRowFormat, "ФИО", "Кол-во правильных ответов", "Диагноз");
-			Console.WriteLine(TableSeparator);
-			foreach (var result in results)
-			{
-				Console.WriteLine(TableRowFormat,
-					result.Name,
-					result.CorrectAnswers,
-					result.Diagnosis);
-			}
-			Console.WriteLine(TableSeparator);
-		}
-
-		static bool AskToShowResults()
-		{
-			while (true)
-			{
-				Console.WriteLine("Показать историю результатов? (да/нет)");
-
-				var userRespond = Console.ReadLine().Trim().ToLower();
-
-				if (userRespond == "да")
-				{
-					return true;
-				}
-				if (userRespond == "нет")
-				{
-					return false;
-				}
-				
-				Console.WriteLine("Введите либо 'да' либо 'нет'!");
-			}
-		}
-	}
+                Console.WriteLine("Введите либо 'да' либо 'нет'!");
+            }
+        }
+    }
 }
