17b2ac59d977729e573cd82fcde463d5bfef7d00
Paul Pomogaev <paulslock1@gmail.com>
2025-04-07 17:09:56 +0400

    Новый проект. Создал разные классы Ball, PointBall, RandomPointBall, RandomSizeAndPointBall и разделил на разные классы

---
+++ BallGamesWinFormsApp/BallGamesWinFormsApp/Ball.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BallGamesWinFormsApp
+{
+    public class Ball
+    {
+        protected MainForm form;
+        protected int x = 150;
+        protected int y = 150;
+        protected int size = 70;
+        public Ball(MainForm form)
+        {
+            this.form = form;
+        }
+
+        public void Show()
+        {
+            var graphics = form.CreateGraphics();
+            var brush = Brushes.Aqua;
+            var rectangle = new Rectangle(x, y, size, size);
+            graphics.FillEllipse(brush, rectangle);
+        }
+    }
+}
