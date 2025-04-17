937c1bcf4d7dce290ca4335b6c10fbd5c778cad6
Paul Pomogaev <paulslock1@gmail.com>
2025-04-17 16:23:42 +0400

    добавил переменную size

--- BallGamesWinFormsApp/BallGamesWinFormsLibrary/Ball.cs
+++ BallGamesWinFormsApp/BallGamesWinFormsLibrary/Ball.cs
@@ -8,6 +8,7 @@ namespace BallGamesWinFormsLibrary
         private Timer timer;
         protected int centerX = 150;
         protected int centerY = 150;
+        protected int size = 70;
         protected int radius = 35;
         protected int vx = 1;
         protected int vy = 1;
