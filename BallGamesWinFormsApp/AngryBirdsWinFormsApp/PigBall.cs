a3e8e58495a8314eb2c28efe6097d57028ba7b50
Paul Pomogaev <paulslock1@gmail.com>
2025-04-29 17:10:44 +0400

    В конструкторе класса PigBall переопределил размер и цвет свинки. В методе RandomPosition генерируется случайное положение с учётом отступов. Не наследовал PigBall от RandomPointBall из-за того, что пришлось бы менять RandomPointBall ввиду необходимости добавить отступы

--- BallGamesWinFormsApp/AngryBirdsWinFormsApp/PigBall.cs
+++ BallGamesWinFormsApp/AngryBirdsWinFormsApp/PigBall.cs
@@ -9,8 +9,20 @@ namespace AngryBirdsWinFormsApp
 {
     public class PigBall : Ball
     {
+        protected static Random random = new Random();
+
         public PigBall(Form form) : base(form)
         {
+            brush = Brushes.Red;
+            Size = 30;
+            radius = Size / 2;
+            RandomPosition();
+        }
+
+        public void RandomPosition()
+        {
+            centerX = random.Next(LeftSide()+50, RightSide()-50);
+            centerY = random.Next(TopSide()+50, DownSide()-100);
         }
     }
 }
