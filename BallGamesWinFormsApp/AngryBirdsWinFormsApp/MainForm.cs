494b8205c16c47ecd5be301fefe103e09fd14abd
Paul Pomogaev <paulslock1@gmail.com>
2025-04-29 17:22:43 +0400

    В MainForm добавил экземпляры классов BirdBall и PigBall, добавил таймер, счётчик попаданий и флаг. Инициализировал экземпляры классов и таймер через метод InitializeGame()

--- BallGamesWinFormsApp/AngryBirdsWinFormsApp/MainForm.cs
+++ BallGamesWinFormsApp/AngryBirdsWinFormsApp/MainForm.cs
@@ -1,10 +1,34 @@
+using Timer = System.Windows.Forms.Timer;
+
 namespace AngryBirdsWinFormsApp
 {
     public partial class MainForm : Form
     {
+        private BirdBall bird;
+        private PigBall pig;
+        private Timer timer = new Timer();
+        private int score = 0;
+        private bool isBirdActive = false;
+
         public MainForm()
         {
             InitializeComponent();
+            InitializeGame();
+        }
+
+        public void InitializeGame()
+        {
+            bird = new BirdBall(this);
+            pig = new PigBall(this);
+
+            timer.Interval = 30;
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
