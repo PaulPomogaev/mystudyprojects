41b9050667499a7d781ec27176d8609920587855
Paul Pomogaev <paulslock1@gmail.com>
2025-04-29 16:36:25 +0400

    Ввёл классы BirdBall и PigBall, в BirdBall внедрил переменные, влияющие на физику: гравитация, потеря импульса при ударе, сопротивление воздуха и трение о землю.

---
+++ BallGamesWinFormsApp/AngryBirdsWinFormsApp/BirdBall.cs
@@ -0,0 +1,21 @@
+﻿using BallGamesWinFormsLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AngryBirdsWinFormsApp
+{
+    public class BirdBall : Ball
+    {
+        private float Gravity = 0.5f;
+        private float GroundStrikeEnergyLoss = 0.7f;
+        private float AirResistance = 0.99f;
+        private float GroundFriction = 0.8f;
+
+        public BirdBall(Form form) : base(form)
+        {
+        }
+    }
+}
---
+++ BallGamesWinFormsApp/AngryBirdsWinFormsApp/PigBall.cs
@@ -0,0 +1,16 @@
+﻿using BallGamesWinFormsLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AngryBirdsWinFormsApp
+{
+    public class PigBall : Ball
+    {
+        public PigBall(Form form) : base(form)
+        {
+        }
+    }
+}
